ed140578659cea38b0d34885ff88c478475e1ad5
Andrea <andreazepeda003@gmail.com>
2018-06-14 11:18:07 -0500

    "fixed the bug with the server"

--- app/models/user.js
+++ app/models/user.js
@@ -1,46 +1,46 @@
-// module.exports = function(sequelize, Sequelize) {
+module.exports = function(sequelize, Sequelize) {
  
-//     var User = sequelize.define('user', {
+    var User = sequelize.define('user', {
  
-//         id: {
-//             autoIncrement: true,
-//             primaryKey: true,
-//             type: Sequelize.INTEGER
-//         },
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
  
-//         firstname: {
-//             type: Sequelize.STRING,
-//             notEmpty: true
-//         },
+        firstname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
  
-//         lastname: {
-//             type: Sequelize.STRING,
-//             notEmpty: true
-//         },
+        lastname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
  
-//         username: {
-//             type: Sequelize.TEXT
-//         },
+        username: {
+            type: Sequelize.TEXT
+        },
  
-//         email: {
-//             type: Sequelize.STRING,
-//             validate: {
-//                 isEmail: true
-//             }
-//         },
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
  
-//         password: {
-//             type: Sequelize.STRING,
-//             allowNull: false
-//         },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
  
-//         last_login: {
-//             type: Sequelize.DATE
-//         },
+        last_login: {
+            type: Sequelize.DATE
+        },
  
       
-//     });
+    });
  
-//     return User;
+    return User;
  
-// }
+}
